a1d5b81aa458fb09d84862f12b02356e01329bbb
hls1793929520 <1793929520@qq.com>
2021-06-30 23:52:34 +0800

    增加键盘敲击

---
+++ src/com/company/tank/T.java
@@ -0,0 +1,10 @@
+package com.company.tank;
+
+import java.awt.*;
+
+public class T {
+    public static void main(String[] args) {
+        TankFrame tankFrame = new TankFrame();
+
+    }
+}
--- src/com/company/tank/TankFrame.java
+++ src/com/company/tank/TankFrame.java
@@ -1,16 +1,22 @@
 package com.company.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
+    int x = 200;
+    int y = 200;
+
     public TankFrame() {
         setSize(800, 600);
         setResizable(false);
         setTitle("tank war");
         setVisible(true);
 
+        this.addKeyListener(new MyKsyListener());
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -20,7 +26,22 @@ public class TankFrame extends Frame {
     }
 
     @Override
-    public void paint(Graphics graphicsg){
-        graphicsg.fillRect(200,200,50,50 );
+    public void paint(Graphics graphicsg) {
+        graphicsg.fillRect(x, y, 50, 50);
+        x = x + 100;
+        y = y + 100;
+    }
+
+    class MyKsyListener extends KeyAdapter {
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println("1");
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("2");
+        }
     }
 }
